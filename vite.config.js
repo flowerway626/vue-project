f89aefad725cbf60a75b7a369728b386a58cc44e
flowerway626 <flowerway626@gmail.com>
2022-12-06 15:40:29 +0800

    更改 base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // 打包前加才可
-  base: '/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
